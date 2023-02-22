11b00914050825fbee467e9c002e58e92922f169
Jaime Núñez <jaime.nunez@outlook.cl>
2023-02-22 19:30:29 -0300

    Initial Commit

---
+++ budget/budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetApp.swift
+//  budget
+//
+//  Created by Jaime Núñez on 22-02-23.
+//
+
+import SwiftUI
+
+@main
+struct budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
